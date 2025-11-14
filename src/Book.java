545e349f36d686a15fde5b97b06342445f152589
Wojciech Napora <naporaw@gmail.com>
2025-11-14 11:29:55 +0100

    Library v0.3

--- src/Book.java
+++ src/Book.java
@@ -7,4 +7,20 @@ public class Book {
     String publisher;
     String isbn;
 
+    Book(String bookTitle, String bookAuthor, int bookRelease,
+         int bookPages, String bookPublisher, String bookIsbn) {
+        title = bookTitle;
+        author = bookAuthor;
+        releaseDate = bookRelease;
+        pages = bookPages;
+        publisher = bookPublisher;
+        isbn = bookIsbn;
+    }
+
+    void printInfo() {
+        String info = title + "; " + author + "; " + releaseDate + "; "
+                + pages + "; " + publisher + "; " + isbn;
+        System.out.println(info);
+    }
+
 }
