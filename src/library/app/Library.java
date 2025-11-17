c12056d29ad407363ee8a334dbca2fb8aa6d42a5
Wojciech Napora <naporaw@gmail.com>
2025-11-17 13:42:56 +0100

    Library v0.8

--- src/library/app/Library.java
+++ src/library/app/Library.java
@@ -1,23 +1,20 @@
 package library.app;
+
+import library.io.DataReader;
 import library.model.Book;
 
 public class Library {
     public static void main(String[] args) {
 
-        final String appName = "Biblioteka v0.7";
+        final String appName = "Biblioteka v0.8";
 
         Book[] books = new Book[1000];
-        books [1] = new Book("W pustyni i w puszczy", "Henryk Sienkiewicz", 2010, 296, "Greg", "9788373271890");
-        books [2] = new Book("Java. Efektywne programowanie. Wydanie II", "Joshua Bloch", 2009, 352, "Helion",
-                "9788324620845");
-        books [3] = new Book("SCJP Sun Certified Programmer for Java 6 Study Guide", "Bert Bates, Katherine Sierra",
-                2008, 851, "McGraw-Hill Osborne Media");
+        DataReader dataReader = new DataReader();
 
         System.out.println(appName);
-        System.out.println("Books available in the library:");
-        books [1].printInfo();
-        books [2].printInfo();
-        books [3].printInfo();
+        System.out.println("Enter a new book:");
+        books[1] = dataReader.readAndCreateBook();
+        books[2] = dataReader.readAndCreateBook();
         System.out.println("The system can store up to " + books.length + " books");
 
     }
---
+++ src/library/io/DataReader.java
@@ -0,0 +1,29 @@
+package library.io;
+
+import library.model.Book;
+
+import java.util.Scanner;
+
+public class DataReader {
+    private Scanner sc = new Scanner(System.in);
+
+    public Book readAndCreateBook() {
+        System.out.println("Title: ");
+        String title = sc.nextLine();
+        System.out.println("Author: ");
+        String author = sc.nextLine();
+        System.out.println("Publishing house: ");
+        String publisher = sc.nextLine();
+        System.out.println("ISBN: ");
+        String isbn = sc.nextLine();
+        System.out.println("Year of publication: ");
+        int releaseDate = sc.nextInt();
+        sc.nextLine();
+        System.out.println("Number of pages: ");
+        int pages = sc.nextInt();
+        sc.nextLine();
+
+        return new Book(title, author, releaseDate, pages, publisher, isbn);
+    }
+
+}
