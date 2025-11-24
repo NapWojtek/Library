b5bad3b10a61bf9e8a6205755a0b5e39e7d3aa93
Wojciech Napora <naporaw@gmail.com>
2025-11-24 20:05:26 +0100

    Library v1.2

--- src/library/model/Book.java
+++ src/library/model/Book.java
@@ -1,6 +1,6 @@
 package library.model;
 
-public class Book extends Publication{
+public class Book extends Publication {
 
     private String author;
     private int pages;
@@ -8,9 +8,7 @@ public class Book extends Publication{
 
     public Book(String title, String author, int year, int pages, String publisher,
                 String isbn) {
-        setTitle(title);
-        setPublisher(publisher);
-        setYear(year);
+        super(title, publisher, year);
         this.pages = pages;
         this.author = author;
         this.isbn = isbn;
