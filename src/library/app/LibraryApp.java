04f3fd1f553ce4930f45e7564f768f8e31cda536
Wojciech Napora <naporaw@gmail.com>
2025-12-07 19:13:54 +0100

    Library v1.7

--- src/library/app/LibraryApp.java
+++ src/library/app/LibraryApp.java
@@ -2,7 +2,7 @@ package library.app;
 
 public class LibraryApp {
 
-    private static final String APP_NAME = "Library v1.6";
+    private static final String APP_NAME = "Library v1.7";
     public static void main(String[] args) {
 
         System.out.println(APP_NAME);
